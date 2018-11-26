52278627dc3e2ec76f8fec63b0027a55a1faac24
weirdo <weidro@weirdos-MacBook-Pro.local>
2018-11-26 15:44:54 +0100

    changes to script test

--- server/integartion_test.sh
+++ server/integartion_test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-for i in `seq 1 20`; do 
-curl -d 'entry=t'${i} -X 'POST' 'http://10.1.0.1:80/board' &
+curl -d 'entry=t'1 -X 'POST' 'http://10.1.0.1:80/board' &
+curl -d 'entry=t'2 -X 'POST' 'http://10.1.0.2:80/board' &
+curl -d 'entry=t'3 -X 'POST' 'http://10.1.0.3:80/board' &
+curl -d 'entry=t'4 -X 'POST' 'http://10.1.0.4:80/board' &
 done
