bbfc2b2b6673916d3c4f1332f7df4e30c334d24e
Oscar Evertsson <oscar.evertsson@live.com>
2018-12-18 15:33:06 +0100

    Implement part 1 and decision making, still missing feature of byzantine.

--- lab4/server/server.py
+++ lab4/server/server.py
@@ -17,17 +17,24 @@ import requests
 # ------------------------------------------------------------------------------------------------------
 try:
     app = Bottle()
+    tot_nodes = 4 # TODO Make this properly
+    status = {} # Step 1, keeps track of votes received, node_id -> vote
+    result_vectors = {} # Step 2, keeps track of vectors received, node_id -> vectors
+    ATTACK = "ATTACK"
+    RETREAT = "RETREAT"
+    BYZANTINE = "BYZANTINE"
 
     # ------------------------------------------------------------------------------------------------------
     # DISTRIBUTED COMMUNICATIONS FUNCTIONS
     # ------------------------------------------------------------------------------------------------------
+
     def contact_vessel(vessel_ip, path, payload=None, req='POST'):
         # Try to contact another server (vessel) through a POST or GET, once
         success = False
         try:
             if 'POST' in req:
                 res = requests.post(
-                    'http://{}{}'.format(vessel_ip, path), data=payload)
+                    'http://{}{}'.format(vessel_ip, path), json=payload)
             elif 'GET' in req:
                 res = requests.get('http://{}{}'.format(vessel_ip, path))
             else:
@@ -49,6 +56,56 @@ try:
                 if not success:
                     print "\n\nCould not contact vessel {}\n\n".format(vessel_id)
 
+    def propogate_client_vote(decision, node_id):
+        path = "/propagate/{}/{}".format(decision, node_id)
+        propagate_to_vessels(path) 
+    
+    def propogate_result(node_id):
+        payload = {'status': status}
+        path = "/propagate/result/{}".format(node_id)
+        propagate_to_vessels(path, payload) 
+
+    def get_result_vector():
+        result_vector = []
+        attack = None
+        retreat = None
+        # Loop over each position in each node vector.
+        for i in range(0, 4):
+            # Count for each position in the final vector, need to reset for each position
+            attack = 0
+            retreat = 0
+            for k, val in result_vectors:
+                if val[i] == ATTACK:
+                    attack += 1
+                else:
+                    retreat += 1
+            # Determine result for each position in the vector.
+            if attack >= retreat:
+                result_vector.append(ATTACK)
+            else:
+                result_vector.append(RETREAT)
+        return result_vector
+
+    def determine_result():
+        result_vector = get_result_vector()
+        result = None
+        attack = 0
+        retreat = 0
+        for v in result_vector:
+            if v == ATTACK:
+                attack += 1
+            else:
+                retreat += 1
+
+        # Decide final winner.
+        if attack >= retreat:
+            result = ATTACK
+        else:
+            result = RETREAT
+        return result, result_vector
+
+                
+
     # ------------------------------------------------------------------------------------------------------
     # ROUTES
     # ------------------------------------------------------------------------------------------------------
@@ -60,26 +117,65 @@ try:
         global node_id
         return template('server/index.tpl', board_title='Vessel {}'.format(node_id), members_name_string='YOUR NAME')
 
+
+
     @app.get('/vote/result')
     def get_result():
         global node_id
-        pass
+        # Have received all other nodes vectors
+        if len(result) == tot_nodes - 1:
+            return determine_result
+
+        # If len (result) == tot_nodes returnera vettigt
+        # Annars returnera att vi väntar 
 
     @app.post('/vote/attack')
     def client_attack_received():
-        # todo
-        pass
+        global node_id
+        status[node_id] = ATTACK
+        propogate_client_vote(ATTACK, node_id)
+        return format_response(200)
 
     @app.post('/vote/retreat')
     def client_retreat_received():
-        # todo
-        pass
+        global node_id
+        status[node_id] = RETREAT
+        propogate_client_vote(RETREAT, node_id)
+        return format_response(200)
 
     @app.post('/vote/byzantine')
     def client_byzantine_received():
-        # todo
-        pass
+        global node_id
+        # TODO MAKE UP YOUR MIND
+        status[node_id] = "Something something" 
+        propogate_client_vote("Something", node_id)
+        return format_response(200)
 
+    # Propagation step 1
+    @app.post('/propagate/<vote>/<external_node_id>')
+    def propagation_received(vote, external_node_id):
+        global node_id
+        status[external_node_id] = vote
+        if len(status) == tot_nodes:
+            propogate_result(node_id)
+        return format_response(200)
+
+    # Propagation step 2
+    @app.post('/propagate/result/<node_id>')
+    def propagation_received(node_id):
+        json_dict = request.json
+        status_dict_for_node = json_dict.get('status')
+        result_vectors[node_id] = status_dict_for_node
+        return format_response(200)
+        
+
+    def format_response(status_code, message=''):
+        '''
+        Simple function for formatting response code.
+        '''
+        if status_code == 200:
+            return HTTPResponse(status=200)
+        return HTTPResponse(status=status_code, body={'message': message})
     # ------------------------------------------------------------------------------------------------------
     # EXECUTION
     # ------------------------------------------------------------------------------------------------------
